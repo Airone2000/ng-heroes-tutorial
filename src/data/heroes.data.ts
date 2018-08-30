d6552beec5ed2fe158465a31bee06815141296ab
Maels1991 <r.wan.guillou+github@gmail.com>
2018-08-30 13:57:07 +0200

    Mock data

---
+++ src/data/heroes.data.ts
@@ -0,0 +1,29 @@
+import { HeroType } from "../types/hero.type";
+
+const HEROES: HeroType[] = [
+    {
+        id: 1,
+        name: "Alphaman",
+        color: "red"
+    },
+    {
+        id: 2,
+        name: "Bravoman",
+        color: "blue"
+    },
+    {
+        id: 3,
+        name: "Charlyman",
+        color: "green"
+    },
+    {
+        id: 4,
+        name: "Deltaman",
+        color: "white"
+    },
+    {
+        id: 5,
+        name: "Echoman",
+        color: "purple"
+    }
+];
---
+++ src/types/hero.type.ts
@@ -0,0 +1,5 @@
+export class HeroType {
+    id: number;
+    name: string;
+    color: string;
+};
